c9b9d17498053cceacb46690ff79414fc89fb246
Sosou222 <szym.baran222@gmail.com>
2025-05-01 18:39:06 +0200

    added extra logic so draggable object properly is destroyed when not in cauldron

--- Assets/Scripts/DraggableItemUI.cs
+++ Assets/Scripts/DraggableItemUI.cs
@@ -7,13 +7,21 @@ using UnityEngine.UI;
 public class DraggableItemUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private CanvasGroup canvasGroup;
+    private Transform beginParent;
     private void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>();
     }
+
+    private void Start()
+    {
+        beginParent = transform.parent;
+        Debug.Log("Begin Parent:" + beginParent.gameObject.name);
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin Drag");
+        transform.SetParent(beginParent, true);
         canvasGroup.blocksRaycasts = false;
     }
 
@@ -27,5 +35,9 @@ public class DraggableItemUI : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     {
         Debug.Log("End Drag");
         canvasGroup.blocksRaycasts = true;
+        if(beginParent == transform.parent)
+        {
+            Destroy(transform.gameObject);
+        }
     }
 }
