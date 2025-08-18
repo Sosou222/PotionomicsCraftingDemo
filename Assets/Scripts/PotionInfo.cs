dee28e686a8aa97f2ab91ada354efb4c4c155cbb
Sosou222 <szym.baran222@gmail.com>
2025-08-18 17:55:07 +0200

    saved last

--- Assets/Scripts/PotionInfo.cs
+++ Assets/Scripts/PotionInfo.cs
@@ -13,18 +13,24 @@ public class PotionInfo : MonoBehaviour
     [SerializeField] private TextMeshProUGUI ratioText;
     [SerializeField] private TextMeshProUGUI potionText;
     
-    public void UpdateInfo(int totalIngidients,int totalMagimins,PotionRecipe recipe)
+    public void UpdateInfo(int totalIngidients,int totalMagimins,string potionName,float starProcentage)
     {
         totalMagiminsText.text = "Total Magimins:" + totalIngidients;
         totalIgridientsText.text = "Total Igridients:" + totalMagimins;
         ratioText.text = totalMagimins + "/60";
-        if (recipe == null)
+        potionText.text = potionName;
+        potionSlider.value = starProcentage;
+        for(int i=0;i< starList.Count; i++)
         {
-            potionText.text = "None";
-        }
-        else
-        {
-            potionText.text = recipe.Name;
+            float baseP = 1.0f/7 * (i+1);
+            if(starProcentage > baseP)
+            {
+                starList[i].GetComponent<Image>().color = Color.white;
+            }
+            else
+            {
+                starList[i].GetComponent<Image>().color = Color.gray;
+            }
         }
 
     }
--- Assets/Scripts/PotionMaker.cs
+++ Assets/Scripts/PotionMaker.cs
@@ -10,6 +10,8 @@ public class PotionMaker : MonoBehaviour
     [SerializeField] private MagiminsLevel magiminsLevel;
 
     [SerializeField] private List<PotionRecipe> recipesList;
+
+    private List<int> recipeLevelCaps = new List<int>() { 60,120,180,240,300,360};
     void Start()
     {
         cauldron.OnItemsChanged += (obj, e) => OnUpdate();
@@ -18,13 +20,10 @@ public class PotionMaker : MonoBehaviour
     private void OnUpdate()
     {
         int total = cauldron.GetTotalMagimins();
-        Debug.Log("Total:" + total);
         foreach (Magimins magimin in (Magimins[])Enum.GetValues(typeof(Magimins)))
         {
             int magCount = cauldron.GetTotalMagiminsOfType(magimin);
             float procentage = (float) magCount/total;
-
-            Debug.Log("Procentage:" + procentage);
 
             magiminsLevel.UpdateMagiminOfType(magimin,procentage, magCount);
         }
@@ -34,7 +33,50 @@ public class PotionMaker : MonoBehaviour
         PotionRecipe recipe = null;
         if (total > 0)
             recipe = MatchClosestPotion();
-        potionInfo.UpdateInfo(totalIgridients, total,recipe);
+
+        float starProcentage = GetStarProcentage();
+        Debug.Log("Procentage:" + starProcentage);
+        string potionLevel = GetPotionLevel();
+        potionInfo.UpdateInfo(totalIgridients, total, potionLevel + recipe.Name, starProcentage);
+    }
+
+    private float GetStarProcentage()
+    {
+        int total = cauldron.GetTotalMagimins();
+        int min = 0;
+        for(int i=1;i< recipeLevelCaps.Count;i++)
+        {
+            if(total > recipeLevelCaps[i])
+            {
+                continue;
+            }
+            else
+            {
+                min = recipeLevelCaps[i-1];
+            }
+        }
+
+
+        float procentage = (float)(min - total)/ min;
+
+        return procentage;
+    }
+
+    public string GetPotionLevel()
+    {
+        string[] names = {"Basic","Normal","Advanced","Grand","Superior","Masterworks" };
+        int index = 0;
+
+        int total = cauldron.GetTotalMagimins();
+        for(int i=0;i<recipeLevelCaps.Count-1;i++)
+        {
+            if (total > recipeLevelCaps[i])
+            {
+                index++;
+            }
+        }
+
+        return names[index];
     }
 
     private PotionRecipe MatchClosestPotion()
